bca5192a8e6d8751108b04a82d2ea06530ebd9f8
Pilwon Huh <pilwon@gmail.com>
2013-04-30 23:42:30 +0900

    Updated routes code with new ensure* interface.

--- server/routes.js
+++ server/routes.js
@@ -8,9 +8,9 @@ var ultimate = require('ultimate');
 
 exports.register = function (app) {
   var csrf = ultimate.server.controller.csrf,
-      ensureLoggedIn = ultimate.server.controller.ensureLoggedIn,
-      ensureLoggedOut = ultimate.server.controller.ensureLoggedOut,
-      ensureRole = ultimate.server.controller.ensureRole;
+      ensureAdmin = ultimate.server.controller.ensureAdmin,
+      ensureGuest = ultimate.server.controller.ensureGuest,
+      ensureUser = ultimate.server.controller.ensureUser;
 
   var error404 = ultimate.server.route.error404,
       restify = ultimate.server.route.restify;
@@ -22,10 +22,10 @@ exports.register = function (app) {
   s.get('/express', c.home.express);
 
   // Account
-  s.get('/account', ensureLoggedIn, c.account.index);
+  s.get('/account', ensureUser, c.account.index);
 
   // Admin
-  s.get('/admin', ensureLoggedIn, ensureRole('admin'), c.admin.index);
+  s.get('/admin', ensureAdmin, c.admin.index);
 
   // API
   restify.any(s, '/api/features', c.api.features, ['list', 'get']);
@@ -34,14 +34,14 @@ exports.register = function (app) {
   restify.admin(s, '/api/admin/features', c.api.features);
 
   // Auth
-  s.get('/login', ensureLoggedOut, c.auth.login);
-  s.post('/login', ensureLoggedOut, csrf, c.auth.loginPOST);
+  s.get('/login', ensureGuest, c.auth.login);
+  s.post('/login', ensureGuest, csrf, c.auth.loginPOST);
   s.get('/logout', c.auth.logout);
   s.post('/logout', c.auth.logoutPOST);
-  s.get('/lost-password', ensureLoggedOut, c.auth.lostPassword);
-  s.post('/lost-password', ensureLoggedOut, csrf, c.auth.lostPasswordPOST);
-  s.get('/register', ensureLoggedOut, c.auth.register);
-  s.post('/register', ensureLoggedOut, csrf, c.auth.registerPOST);
+  s.get('/lost-password', ensureGuest, c.auth.lostPassword);
+  s.post('/lost-password', ensureGuest, csrf, c.auth.lostPasswordPOST);
+  s.get('/register', ensureGuest, c.auth.register);
+  s.post('/register', ensureGuest, csrf, c.auth.registerPOST);
   s.get('/auth/facebook', c.auth.facebook);
   s.get('/auth/facebook/callback', c.auth.facebookCallback);
   s.get('/auth/facebook/success', c.auth.facebookSuccess);
