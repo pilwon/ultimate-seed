533d9f43951bdf1bb4a943876140226e65be5ff3
Pilwon Huh <pilwon@gmail.com>
2013-05-01 00:50:38 +0900

    Improved readability of winston app exception messages.

--- server/winston.js
+++ server/winston.js
@@ -48,16 +48,9 @@ function _attachExpressLogger() {
 
   server.use(expressWinston.logger({
     transports: [
-      // new (winston.transports.Console)({
-      //   colorize: true,
-      //   handleExceptions: true,
-      //   label: 'express',
-      //   level: logLevels.console
-      // }),
       new (winston.transports.File)({
         level: logLevels.file,
         filename: path.join(_app.project.path.log, 'express.log'),
-        handleExceptions: true,
         timestamp: true
       }),
       new (winstonMongoDB.MongoDB)({
@@ -90,14 +83,12 @@ function _attachExpressErrorLogger() {
     transports: [
       new (winston.transports.Console)({
         colorize: true,
-        handleExceptions: true,
         label: 'express.error',
         level: logLevels.console
       }),
       new (winston.transports.File)({
         level: logLevels.file,
         filename: path.join(_app.project.path.log, 'express-error.log'),
-        handleExceptions: true,
         timestamp: true
       }),
       new (winstonMongoDB.MongoDB)({
@@ -122,31 +113,54 @@ function attach(app) {
     mongodb: 'error'
   };
 
+  var host = _vcapMongo.hostname || _app.config.db.mongo.host || 'localhost',
+      port = _vcapMongo.port || _app.config.db.mongo.port || 27017,
+      username = _vcapMongo.username || _app.config.db.mongo.username || null,
+      password = _vcapMongo.password || _app.config.db.mongo.password || null,
+      db = _vcapMongo.db || _app.config.db.mongo.db || 'ultimate';
+
   if (process.env.NODE_ENV === 'development') {
     logLevels.console = 'debug';
   }
 
   _app.logger = new (winston.Logger)({
+    exceptionHandlers: [
+      new (winston.transports.Console)({
+        json: true
+      }),
+      new (winston.transports.File)({
+        level: logLevels.file,
+        filename: path.join(_app.project.path.log, 'exceptions.log'),
+        timestamp: true
+      }),
+      new (winstonMongoDB.MongoDB)({
+        level: logLevels.mongodb,
+        host: host,
+        port: port,
+        username: username,
+        password: password,
+        db: db,
+        collection: 'log-exceptions'
+      })
+    ],
     transports: [
       new (winston.transports.Console)({
         colorize: true,
-        handleExceptions: true,
         label: 'app',
         level: logLevels.console
       }),
       new (winston.transports.File)({
         level: logLevels.file,
         filename: path.join(_app.project.path.log, 'app.log'),
-        handleExceptions: true,
         timestamp: true
       }),
       new (winstonMongoDB.MongoDB)({
         level: logLevels.mongodb,
-        host: _vcapMongo.hostname || _app.config.db.mongo.host || 'localhost',
-        port: _vcapMongo.port || _app.config.db.mongo.port || 27017,
-        username: _vcapMongo.username || _app.config.db.mongo.username || null,
-        password: _vcapMongo.password || _app.config.db.mongo.password || null,
-        db: _vcapMongo.db || _app.config.db.mongo.db || 'ultimate',
+        host: host,
+        port: port,
+        username: username,
+        password: password,
+        db: db,
         collection: 'log-app'
       })
     ]
