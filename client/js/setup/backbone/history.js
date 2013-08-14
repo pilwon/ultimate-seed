832d24a771bb84e13804cfbf55e19cf7691dd6d0
Pilwon Huh <pilwon@gmail.com>
2013-08-14 12:17:39 -0400

    Added {refresh: true} option for Backbone.history.

---
+++ client/js/setup/backbone/history.js
@@ -0,0 +1,22 @@
+/*
+ * client/js/setup/backbone/history.js
+ */
+
+'use strict';
+
+var _ = require('lodash'),
+    $ = require('jquery'),
+    Backbone = require('backbone');
+
+var _navigate = Backbone.History.prototype.navigate;
+
+Backbone.History.prototype.navigate = function (fragment, options) {
+  var prevFragment = this.fragment;
+
+  _navigate.call(this, fragment, options);
+
+  if (options.refresh && options.trigger &&
+      prevFragment === this.getFragment(fragment)) {
+    this.loadUrl(fragment);
+  }
+};
