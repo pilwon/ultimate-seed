ebb42197e785170778ba35c52f47028a868dd4c2
Pilwon Huh <pilwon@gmail.com>
2013-08-16 00:28:31 -0400

    Added missing server-side lib.controller.index.

--- server/lib/controller.js
+++ server/lib/controller.js
@@ -12,9 +12,14 @@ var _clientDir = path.join(app.dir, '..', app.project.path[
   process.env.NODE_ENV === 'development' ? 'client' : 'dist'
 ]);
 
+function index(req, res) {
+  res.sendfile(path.join(_clientDir, 'index.html'));
+}
+
 function error404(req, res) {
   res.status(404).sendfile(path.join(_clientDir, '404.html'));
 }
 
 // Public API
+exports.index = index;
 exports.error404 = error404;
