6b867808f71b990d1aaf4befad178c3df439f186
Pilwon Huh <pilwon@gmail.com>
2013-09-06 02:34:25 -0400

    Added scrollTop support for app.navigate

--- client/js/setup/backbone/history.js
+++ client/js/setup/backbone/history.js
@@ -4,12 +4,25 @@
 
 'use strict';
 
-var Backbone = require('backbone');
+var _ = require('lodash'),
+    Backbone = require('backbone');
 
 var _navigate = Backbone.History.prototype.navigate;
 
 Backbone.History.prototype.navigate = function (fragment, options) {
+  if (!_.isPlainObject(options)) { options = {}; }
+
   var prevFragment = this.fragment;
+
+  _.defaults(options, {
+    trigger: false,
+    replace: false,
+    scrollTop: true
+  });
+
+  if (options.scrollTop) {
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
+  }
 
   _navigate.apply(this, arguments);
 
