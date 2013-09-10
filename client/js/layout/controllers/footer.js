ef04cd07f3b0e85c914927b78b7ba0ae8fd91fd3
Pilwon Huh <pilwon@gmail.com>
2013-09-10 13:34:07 -0400

    Added livereload support.

--- client/js/layout/controllers/footer.js
+++ client/js/layout/controllers/footer.js
@@ -4,8 +4,14 @@
 
 'use strict';
 
-exports = module.exports = function (ngModule) {
-  ngModule.controller('FooterCtrl', function () {
+var util = require('util');
 
+var $ = require('jquery');
+
+exports = module.exports = function (ngModule) {
+  ngModule.controller('FooterCtrl', function ($scope, $cookies) {
+    if ($cookies.livereload) {
+      $.getScript(util.format('http://localhost:%s/livereload.js?snipver=1', $cookies.livereload));
+    }
   });
 };
