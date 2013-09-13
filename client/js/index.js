17e7f264547f949b3ea90aacc1f283b3bddbbc13
Pilwon Huh <pilwon@gmail.com>
2013-09-12 22:24:01 -0400

    Added socket.io code.

--- client/js/index.js
+++ client/js/index.js
@@ -4,6 +4,10 @@
 
 'use strict';
 
+var _ = require('lodash'),
+    $ = require('jquery'),
+    socketio = require('socketio');
+
 // Load dependencies.
 require('angular.translate');
 require('angular.ui');
@@ -20,3 +24,43 @@ require('./shared');
 
 // Register app.
 require('./app');
+
+// Connect to socket.io server.
+$(function () {
+  var retryInterval = 5000,
+      retryTimer;
+
+  (function connect() {
+    clearInterval(retryTimer);
+
+    var socket = global.socket = socketio.connect('', {
+      'force new connection': true,
+      'max reconnection attempts': Infinity,
+      'reconnection limit': 10 * 1000
+    });
+
+    socket.on('connect', function () {
+      socket.emit('info', {
+        // modernizr: Modernizr,
+        navigator: _.transform(navigator, function (result, val, key) {
+          if (_.isString(val)) {
+            result[key] = val;
+          }
+        })
+      });
+    });
+
+    socket.on('test', function (data) {
+      console.log(data);
+      socket.emit('test', { hello: 'from browser world' });
+    });
+
+    retryTimer = setInterval(function () {
+      if (!socket.socket.connected &&
+          !socket.socket.connecting &&
+          !socket.socket.reconnecting) {
+        connect();
+      }
+    }, retryInterval);
+  }());
+});
