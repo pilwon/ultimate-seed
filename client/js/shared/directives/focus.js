ed419d6e923ce4f5864ff784d3843c274d987d45
Brian Park <yaru22@gmail.com>
2013-09-27 16:21:35 -0400

    Un-nested the function so as to conform to the project's coding style.

--- client/js/shared/directives/focus.js
+++ client/js/shared/directives/focus.js
@@ -4,19 +4,27 @@
 
 'use strict';
 
+var _injected = {};
+
+function link(scope, elem, attrs) {
+  scope.$watch(attrs.focus, function () {
+    var getter = _injected.$parse(attrs.focus);
+    var setter = getter.assign;
+    if (getter(scope)) {
+      elem[0].focus();
+      setter(scope, false);
+    }
+  });
+}
+
 exports = module.exports = function (ngModule) {
   ngModule.directive('focus', function ($parse) {
+    _injected = {
+      $parse: $parse
+    };
+
     return {
-      link: function (scope, elem, attrs) {
-        scope.$watch(attrs.focus, function() {
-          var getter = $parse(attrs.focus);
-          var setter = getter.assign;
-          if (getter(scope)) {
-            elem[0].focus();
-            setter(scope, false);
-          }
-        });
-      }
+      link: link
     };
   });
 };
