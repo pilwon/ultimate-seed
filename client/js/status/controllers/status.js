b61117215855e2bc010cf291fea17d95dd00b81c
Tobias Kopelke <nox@raynode.de>
2013-10-28 09:33:01 +0100

    simplified the status items and reversed the order

--- client/js/status/controllers/status.js
+++ client/js/status/controllers/status.js
@@ -7,11 +7,9 @@
 exports = module.exports = function (ngModule) {
   ngModule.controller('StatusCtrl', function ($scope, $http) {
     $scope.status = [];
-    var index = 0;
     $scope.fetch = function() {
       $http.get('/status/health').then(function (res) {
-        $scope.status[index++] = res.data;
-        return res.data;
+        $scope.status.unshift(res.data);
       });
     };
     $scope.fetch();
